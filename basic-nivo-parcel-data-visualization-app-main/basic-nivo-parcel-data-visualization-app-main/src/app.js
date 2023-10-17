f742a306e91a74d94889ae4d113b8e93e66e44f5
samypaul-admin <stpaul@crimson.ua.edu>
2023-10-17 10:11:57 -0500

    update

--- basic-nivo-parcel-data-visualization-app-main/basic-nivo-parcel-data-visualization-app-main/src/app.js
+++ basic-nivo-parcel-data-visualization-app-main/basic-nivo-parcel-data-visualization-app-main/src/app.js
@@ -1,36 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import json_data from './data/data'
-import json_data2 from './data/data2'
-import LineGraph from './components/LineGraph';
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import {baseTheme} from './assets/global/Theme-variable'
+import Themeroutes from "./routes/Router";
 
-let root = null;
-
-let createEventListener = function (id, callback) {
-    document.getElementById(id).onclick = callback;
+const App = () => {
+  const routing = useRoutes(Themeroutes);
+  const theme = baseTheme;
+  return (
+    <ThemeProvider theme={theme}>
+      {routing}
+    </ThemeProvider>
+  );
 };
 
-
-let handleEvents = function () {
-    createEventListener("d1", () => {updateData(json_data)});
-    createEventListener("d2", () => {updateData(json_data2)});
-};
-
-// Fix for getBBox error https://github.com/plouc/nivo/issues/2162#issuecomment-1467184517
-HTMLCanvasElement.prototype.getBBox = function () {
-    return { width: this.offsetWidth, height: this.offsetHeight };
-};
-
-let initData = function(){
-    let data = [ json_data ]
-    root = createRoot(document.getElementById('component'))
-    root.render(<LineGraph data={data} />)
-}
-
-let updateData = function (dataset) {
-    let data = [ dataset ]
-    root.render(<LineGraph data={data} />)
-    console.log("loading")
-}
-
-initData()
-handleEvents()
+export default App;
